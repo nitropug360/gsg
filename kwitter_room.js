311944e09ece8df5e10cffc875184f4521fe8685
nitropug360 <85816373+nitropug360@users.noreply.github.com>
2022-08-08 12:06:03 +1200

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var un = localStorage.getItem("username");
+document.getElementById("welcome").innerHTML="Welcome "+ un;
+//ADD YOUR FIREBASE LINKS HERE
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
